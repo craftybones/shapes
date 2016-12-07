e695d0583745a0c9c0ec66170e57a7f825ccaf60
Srijayanth Sridhar <srijayanth@gmail.com>
2016-12-07 19:03:42 +0530

    Added a bordered line function

--- src/shapes/line.clj
+++ src/shapes/line.clj
@@ -2,4 +2,17 @@
   (:require [shapes.utils :refer :all])
   (:gen-class))
 
-(def line (flip repeat-string))
+(def line
+  "creates a string of repeated elements"
+  (flip repeat-string))
+
+(def empty-line
+  "creates a string of spaces"
+  (partial line " "))
+
+(defn bordered-line [c n]
+  "creates a string of two c's separated by n-2 spaces"
+  (condp = n
+    0 ""
+    1 c
+    (str c (empty-line (- n 2)) c)))
--- test/shapes/line_test.clj
+++ test/shapes/line_test.clj
@@ -5,6 +5,15 @@
 (deftest line-test
   (let [line-of-stars (partial line "*")]
     (testing "A single line of stars"
+      (is (= "" (line-of-stars 0)))
       (is (= "*" (line-of-stars 1)))
       (is (= "**" (line-of-stars 2)))
       (is (= "***" (line-of-stars 3))))))
+
+(deftest bordered-line-test
+  (let [bordered-star-line (partial bordered-line "*")]
+    (testing "A single hollow line bordered by stars"
+      (is (= "" (bordered-star-line 0)))
+      (is (= "*" (bordered-star-line 1)))
+      (is (= "**" (bordered-star-line 2)))
+      (is (= "* *" (bordered-star-line 3))))))
